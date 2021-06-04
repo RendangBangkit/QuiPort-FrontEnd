8b26314b166f887d65a94630b47de80ba1984f48
rifkiilmi <rifkiilmi98@gmail.com>
2021-06-04 14:25:03 +0700

    Setup Requirements

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
+import React, { Component } from 'react'
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Axios from "axios"
+import { Container, Nav, Row, Col, Tab } from "react-bootstrap";
+
+
+
+class App extends Component{
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      myProjects: [],
+      starred: [],
+      createNew: []
+    }
+  }
+
+  // componentDidMount() {
+  //   this.getMyProjects()
+  //   this.getStarredProjects()
+  // }
+
+  render() {
+    return (
+      <Container fluid className="p-3">
+        <Tab.Container id="left-tabs-example" defaultActiveKey="first">
+          <Row>
+            <Col sm={2}>
+              <Nav variant="pills" className="flex-column">
+                <Nav.Item>
+                  <Nav.Link eventKey="first">My Projects</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="second">Starred Projects</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="third">Create a New Project</Nav.Link>
+                </Nav.Item>
+              </Nav>
+            </Col>
+            
+          </Row>
+        </Tab.Container>
+    </Container>
+    );
+  }
 }
 
 export default App;
